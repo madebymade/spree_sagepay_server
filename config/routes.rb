7477ae7945dd59940e5af6727d6bb1d3e36ed2bd
Chris Blackburn <christopher.blackburn@gmail.com>
2015-02-18 16:33:41 +0000

    Use draw method instead of append to prevent routes being appended multiple times

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.append do
+Spree::Core::Engine.routes.draw do
   scope :sagepay do
     post :notification, to: 'sagepay#notification', as: 'sagepay_notification'
     get :redirect, to: 'sagepay#redirect', as: 'sagepay_redirect'
